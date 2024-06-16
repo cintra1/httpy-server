dcacef60b2190cc2300ac211afdb4d157d93aef6
Mateus Cintra <mateus.cintra@tembici.com>
2024-06-16 18:45:05 -0300

    feat: add 200 OK

--- app/main.py
+++ app/main.py
@@ -5,9 +5,8 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
-    server_socket.sendmsg("HTTP/1.1 200 OK\r\n\r\n")
+    if server_socket.accept(): # wait for client
+         socket.create_connection()
 
 if __name__ == "__main__":
     main()
