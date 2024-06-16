e6fbd684dcef2ecdb385b2eeba3d714691e0f296
Mateus Cintra <mateus.cintra@tembici.com>
2024-06-16 18:43:07 -0300

    feat: add 200 OK

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept() # wait for client
 
+    server_socket.sendmsg("HTTP/1.1 200 OK\r\n\r\n")
 
 if __name__ == "__main__":
     main()
