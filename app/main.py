c7f8036099692aa76019ae36ff20818858ad7014
Mateus Cintra <mateus.cintra@tembici.com>
2024-06-16 19:26:22 -0300

    feat: add 200 OK

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def main():
     try:
         while True:
             print("Waiting for a connection...")
-            a = server_socket.getaddrinfo()
+            a = socket.getaddrinfo()
             print("liste",a)
             conn, addr = server_socket.accept()
             print('Connected by', addr)
