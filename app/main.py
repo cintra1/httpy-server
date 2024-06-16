5c48f8b3a7996a7a9af07b16203b79f0dfe761c3
Mateus Cintra <mateus.cintra@tembici.com>
2024-06-16 20:08:37 -0300

    feat: add 400 error

--- app/main.py
+++ app/main.py
@@ -8,9 +8,16 @@ def handle_request(conn):
         request_line = lines[0]
         method, path, http_version = request_line.split()
 
+    method = method.decode()
+    path = path.decode()
+    http_version = http_version.decode()
+
     print(f"Metodo {method}, path: {path}, version: {http_version}")
     
-    if path.decode() == "/":
+    if path[:5] == "/echo":
+        str = path[6:]
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(str)}\r\n\r\n{str}"
+    if path == "/":
         response = "HTTP/1.1 200 OK\r\n\r\n"
     else:
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
