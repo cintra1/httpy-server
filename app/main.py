bdccf98281a947d809c540a302f90c8b28296a81
Mateus Cintra <mateus.cintra@tembici.com>
2024-06-16 20:14:51 -0300

    feat: add 400 error

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def handle_request(conn):
 
     print(f"Metodo {method}, path: {path}, version: {http_version}")
 
-    if path[:5] == "/echo":
+    if path.startswith("/echo"):
         str = path[6:]
         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(str)}\r\n\r\n{str}"
     elif path == "/":
