eddb0bbdce8301811883cd04191ae0c215fcd4d7
Mateus Cintra <mateus.cintra@tembici.com>
2024-06-17 11:14:30 -0300

    feat: add compression support

--- app/main.py
+++ app/main.py
@@ -53,7 +53,7 @@ def handle_request(conn):
         if len(lines) >= 3:
             request_line = lines[0].decode()
             text = lines[-1].decode()
-            header_key, header_value = text.decode().split(': ')
+            header_key, header_value = text.split(': ')
         str = path[6:]
         if header_value == "gzip":
             response = f"HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: {len(str)}\r\n\r\n{str}"
